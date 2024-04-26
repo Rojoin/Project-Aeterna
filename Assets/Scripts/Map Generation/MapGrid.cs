65ff6c31545a9ef416a967a831d3f8a6749ae2be
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2024-04-26 19:48:30 -0300

    Add list chambers and names

--- Assets/Scripts/Map Generation/MapGrid.cs
+++ Assets/Scripts/Map Generation/MapGrid.cs
@@ -1,8 +1,36 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGrid : MonoBehaviour
 {
+    [SerializeField] private Room[] rooms;
 
+    private void Start()
+    {
+
+    }
+
+    private void SetRoom()
+    {
+
+    }
+
+}
+
+public enum RoomType
+{
+    I,
+    L,
+    X,
+    T,
+    End,
+    Start,
+}
+
+[Serializable]
+public class Room
+{
+    public GameObject prefabRoom;
+    public RoomType type;
 }
