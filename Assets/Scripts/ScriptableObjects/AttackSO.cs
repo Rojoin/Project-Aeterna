8d8d3ef84f2a3271a065c922aa8a11494dd3b9c8
Rojoin <nachoarrastua@gmail.com>
2024-06-01 17:38:45 -0300

    Change AttackSO to play the animation in the SO

--- Assets/Scripts/ScriptableObjects/AttackSO.cs
+++ Assets/Scripts/ScriptableObjects/AttackSO.cs
@@ -5,11 +5,16 @@ namespace ScriptableObjects
     [CreateAssetMenu(fileName = "AttackFileSO", menuName = "Attacks" , order = 0)]
     public class AttackSO : ScriptableObject
     {
-        public AnimatorOverrideController overrideController;
+        public string animationName;
         public float damage;
         public Vector3 colliderCenter;
         public Vector3 colliderSize;
         public float attackTime;
         public float timeUntilStart;
+        
+        public float timeUntilEnd;
+        
     }
+    //Todo: Make a way for multihit attacks.
+    //Maybe make a custom method inside the attackSo
 }
