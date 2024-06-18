48a782c6f93256e11a21f1ac21667353f68d0bfe
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2024-06-18 18:31:29 -0300

    Add Sfx

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,15 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID DOOR_CLOSE = 128625444U;
+        static const AkUniqueID DOOR_OPEN = 535830432U;
         static const AkUniqueID DUMMY_DEAD = 2103557112U;
         static const AkUniqueID DUMMY_HIT = 2333910817U;
         static const AkUniqueID EVENT_TEST = 1618187162U;
+        static const AkUniqueID PLAYER_ATTACK_SWORD = 4161660071U;
+        static const AkUniqueID PLAYER_DEATH = 3083087645U;
+        static const AkUniqueID PLAYER_RECIEVEDAMAGE = 1990870923U;
+        static const AkUniqueID PLAYER_WALK_LEVEL1 = 3808387538U;
     } // namespace EVENTS
 
     namespace BANKS
