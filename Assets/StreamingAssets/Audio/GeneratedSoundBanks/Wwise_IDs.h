9724aaa956cb63be5df803b0b57e9afbf7276670
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2024-09-13 14:58:18 -0300

    Add dashSound

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -20,6 +20,7 @@ namespace AK
         static const AkUniqueID EVENT_TEST = 1618187162U;
         static const AkUniqueID PLAY_DEFAULTMUSIC = 1857418338U;
         static const AkUniqueID PLAYER_ATTACK_SWORD = 4161660071U;
+        static const AkUniqueID PLAYER_DASH = 2394582229U;
         static const AkUniqueID PLAYER_DEATH = 3083087645U;
         static const AkUniqueID PLAYER_RECIEVEDAMAGE = 1990870923U;
         static const AkUniqueID PLAYER_WALK_LEVEL1 = 3808387538U;
