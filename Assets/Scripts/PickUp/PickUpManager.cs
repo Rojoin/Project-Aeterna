d4517214587760b59d17805851c031021ff1b6e8
fekox <facusantos123@gmail.com>
2024-10-03 17:22:37 -0300

    Implement the pick up logic

--- Assets/Scripts/PickUp/PickUpManager.cs
+++ Assets/Scripts/PickUp/PickUpManager.cs
@@ -9,10 +9,29 @@ public class PickUpManager : MonoBehaviour
     [Header("Prefab to spawn")]
     [SerializeField] private CharacterController player;
 
+    [Header("Select Card Menu")]
+    [SerializeField] private SelectCardMenu selectCardMenu;
+
+    private GameObject prefab;
+    private PickUpCollider pickUpCollider;
+
     public IEnumerator SpawnPickUp(int time) 
     {
         yield return new WaitForSeconds(time);
 
-        Instantiate(pickUpPrefab, new Vector3(player.transform.position.x - 1.5f, player.transform.position.y + 1, player.transform.position.z), Quaternion.identity);
+        prefab = Instantiate(pickUpPrefab, new Vector3(player.transform.position.x - 2f, player.transform.position.y + 1, player.transform.position.z), Quaternion.identity);
+        pickUpCollider = prefab.GetComponent<PickUpCollider>();
+        pickUpCollider.onPlayerInteractPickUp.AddListener(PlayerInteractPickUp);
     } 
+
+    public void PlayerInteractPickUp() 
+    {
+        Debug.Log("Evento LLamado");
+        selectCardMenu.ShowSelectCardMenu(true);
+    }
+
+    private void OnDestroy()
+    {
+        pickUpCollider.onPlayerInteractPickUp.RemoveListener(PlayerInteractPickUp);
+    }
 }
