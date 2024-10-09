ab35922b79fb209e4b48b85123fe9c37914269c7
Rojoin <nachoarrastua@gmail.com>
2024-10-08 21:34:23 -0300

    Add GameManager prefab

--- Assets/Scripts/PickUp/PickUpManager.cs
+++ Assets/Scripts/PickUp/PickUpManager.cs
@@ -3,30 +3,33 @@ using UnityEngine;
 
 public class PickUpManager : MonoBehaviour
 {
-    [Header("Prefab to spawn")]
-    [SerializeField] private GameObject pickUpPrefab;
+    [Header("Prefab to spawn")] [SerializeField]
+    private GameObject pickUpPrefab;
 
-    [Header("Prefab to spawn")]
-    [SerializeField] private CharacterController player;
+    [Header("Prefab to spawn")] [SerializeField]
+    private CharacterController player;
 
-    [Header("Select Card Menu")]
-    [SerializeField] private SelectCardMenu selectCardMenu;
+    [Header("Select Card Menu")] [SerializeField]
+    private SelectCardMenu selectCardMenu;
 
-    [Header("Restart time speed")]
-    [SerializeField] private float restartTimeSpeed = 0.4f;
+    [Header("Restart time speed")] [SerializeField]
+    private float restartTimeSpeed = 0.4f;
 
     private GameObject prefab;
     private PickUpCollider pickUpCollider;
     private bool stopTime;
 
-    public IEnumerator SpawnPickUp(int time) 
+    public IEnumerator SpawnPickUp(int time)
     {
+        //TODO: Change using navmesh to select the point from the radius of the player
         Time.timeScale = 0.1f;
         stopTime = true;
 
         yield return new WaitForSeconds(time);
 
-        prefab = Instantiate(pickUpPrefab, new Vector3(player.transform.position.x - 2f, player.transform.position.y + 1, player.transform.position.z), Quaternion.identity);
+        prefab = Instantiate(pickUpPrefab,
+            new Vector3(player.transform.position.x - 2f, player.transform.position.y + 1, player.transform.position.z),
+            Quaternion.identity);
         pickUpCollider = prefab.GetComponent<PickUpCollider>();
         pickUpCollider.onPlayerInteractPickUp.AddListener(PlayerInteractPickUp);
     }
@@ -36,21 +39,21 @@ public class PickUpManager : MonoBehaviour
         RestartTime();
     }
 
-    public void RestartTime() 
+    public void RestartTime()
     {
-        if (stopTime) 
+        if (stopTime)
         {
             Time.timeScale += restartTimeSpeed * Time.deltaTime;
         }
 
-        if(Time.timeScale >= 1) 
+        if (Time.timeScale >= 1)
         {
             Time.timeScale = 1f;
             stopTime = false;
         }
     }
 
-    public void PlayerInteractPickUp() 
+    public void PlayerInteractPickUp()
     {
         Debug.Log("Evento LLamado");
         selectCardMenu.ShowSelectCardMenu(true);
