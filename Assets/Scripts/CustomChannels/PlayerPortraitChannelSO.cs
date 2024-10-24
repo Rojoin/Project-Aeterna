43e36a1b7ec0c966fd084821e6d5f80bffe3973c
Rojoin <nachoarrastua@gmail.com>
2024-10-24 03:55:00 -0300

    Add Face reaction when hurt

---
+++ Assets/Scripts/CustomChannels/PlayerPortraitChannelSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace CustomChannels
+{
+    [CreateAssetMenu(menuName = "Channels/PlayerPortrait", fileName = "PlayerPortraitChannelSO", order = 0)]
+    public class PlayerPortraitChannelSO : ChannelSO<UI.PlayerPortraitStates>
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/PlayerPortrait.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using Coffee.UIEffects;
+using CustomChannels;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public enum PlayerPortraitStates
+    {
+        None,
+        Normal,
+        InBattle,
+        Hit,
+        LowHealth
+    }
+
+    public class PlayerPortrait : MonoBehaviour
+    {
+        public PlayerPortraitChannelSO OnPortraitChangeChannel;
+        [SerializeField] private float whiteEffectTime = 0.2f;
+        public AnimationCurve whiteAnimationCurve;
+        private PlayerPortraitStates currentState = PlayerPortraitStates.Normal;
+        public Sprite defaultPortrait;
+        public Sprite inBattlePortrait;
+        public Sprite hitPortrait;
+        public Sprite lowHealthPortrait;
+        private Image portrait;
+        private UIEffect effectPortrait;
+        private Coroutine hitPlayer;
+        private Coroutine lowHealthPlayer;
+
+        void OnEnable()
+        {
+            portrait = GetComponent<Image>();
+            effectPortrait = GetComponent<UIEffect>();
+            portrait.sprite = defaultPortrait;
+            OnPortraitChangeChannel.Subscribe(ChangePortrait);
+        }
+
+        void ONDisable()
+        {
+            OnPortraitChangeChannel.Unsubscribe(ChangePortrait);
+        }
+
+        private void ChangePortrait(PlayerPortraitStates obj)
+        {
+            switch (obj)
+            {
+                case PlayerPortraitStates.None:
+                    break;
+                case PlayerPortraitStates.Normal:
+                    if (currentState != PlayerPortraitStates.LowHealth)
+                    {
+                        portrait.sprite = defaultPortrait;
+                        currentState = PlayerPortraitStates.Normal;
+                    }
+
+                    break;
+                case PlayerPortraitStates.InBattle:
+                    portrait.sprite = inBattlePortrait;
+                    currentState = PlayerPortraitStates.InBattle;
+                    break;
+                case PlayerPortraitStates.Hit:
+                    OnPlayerHit();
+                    break;
+                case PlayerPortraitStates.LowHealth:
+                    portrait.sprite = lowHealthPortrait;
+                    currentState = PlayerPortraitStates.LowHealth;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(obj), obj, null);
+            }
+        }
+
+        private void OnPlayerHit()
+        {
+            portrait.sprite = hitPortrait;
+
+            if (hitPlayer != null)
+            {
+                StopCoroutine(hitPlayer);
+            }
+
+            hitPlayer = StartCoroutine(PlayerHitAnim());
+        }
+
+        public IEnumerator PlayerHitAnim()
+        {
+            float timer = 0;
+            while (timer < whiteEffectTime)
+            {
+                timer += Time.deltaTime;
+                effectPortrait.colorFactor = whiteAnimationCurve.Evaluate(timer / whiteEffectTime);
+                yield return null;
+            }
+
+            effectPortrait.effectFactor = 0.0f;
+            ChangePortrait(currentState);
+        }
+    }
+}
