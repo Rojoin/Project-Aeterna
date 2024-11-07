713da464b4f2c32948f1c5fcff32cd4101c4d1a8
Rojoin <nachoarrastua@gmail.com>
2024-11-07 00:27:25 -0300

    Add EnemyPosition Channel

---
+++ Assets/Scripts/UI/UIParticleController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Coffee.UIExtensions;
+using UnityEngine;
+
+namespace UI
+{
+    public class UIParticleController : MonoBehaviour
+    {
+        [SerializeField] private Vector3ChannelSO OnParticleCalled;
+        private RectTransform rectTransform;
+        private UIParticle particleSystem;
+        [SerializeField] private Canvas canvas;
+
+        public void OnEnable()
+        {
+            OnParticleCalled.Subscribe(SpawnParticle);
+            rectTransform = GetComponent<RectTransform>();
+            particleSystem = GetComponent<UIParticle>();
+        }
+
+        private void OnDisable()
+        {
+            OnParticleCalled.Unsubscribe(SpawnParticle);
+        }
+
+        private void SpawnParticle(Vector3 obj)
+        {
+            Vector2 screenPosition = Camera.main.WorldToScreenPoint(obj);
+
+          
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), screenPosition,Camera.main ,out Vector2 canvasPosition);
+
+
+            rectTransform.anchoredPosition = canvasPosition;
+            particleSystem.Play();
+        }
+    }
+}
