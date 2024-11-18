639fb3001cb7aa7154c8df611a8322a8d4bf3e55
Rojoin <nachoarrastua@gmail.com>
2024-11-18 15:38:02 -0300

    Fix Inputs Icon

--- Assets/Scripts/Menu/Menu/TutorialController.cs
+++ Assets/Scripts/Menu/Menu/TutorialController.cs
@@ -1,15 +1,24 @@
+using System;
 using CustomSceneSwitcher.Switcher;
 using CustomSceneSwitcher.Switcher.Data;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TutorialController : MonoBehaviour
 {
     [SerializeField] private SceneChangeData GoToGame;
+    [SerializeField] private Button button;
 
     public void LoadGame() 
     {
         SceneSwitcher.ChangeScene(GoToGame);
+    }
+
+    public void Update()
+    {
+        EventSystem.current.SetSelectedGameObject(button.gameObject);
     }
 }
