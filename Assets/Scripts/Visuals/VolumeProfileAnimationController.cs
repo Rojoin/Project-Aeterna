b2d29312827cdfbb42b0985122f567b16cac0a50
Rojoin <nachoarrastua@gmail.com>
2024-12-06 03:54:30 -0300

    Add VolumeProfile Controller

---
+++ Assets/Scripts/Visuals/VolumeProfileAnimationController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace Visuals
+{
+    public class VolumeProfileAnimationController : MonoBehaviour
+    {
+        private static readonly int OnChamberComplete = Animator.StringToHash("OnChamberComplete");
+        private static readonly int OnCardSpawn = Animator.StringToHash("OnCardSpawn");
+        private static readonly int OnPlayerDeath = Animator.StringToHash("OnPlayerDeath");
+        public VoidChannelSO onCardSpawn;
+        public VoidChannelSO onChamberComplete;
+        public BoolChannelSO onDeath;
+        private Animator animator;
+
+
+        private void OnEnable()
+        {
+            onCardSpawn.Subscribe(ActivateSpawnAnimation);
+            onDeath.Subscribe(ActivateDeathAnimation);
+            onChamberComplete.Subscribe(ActivateChamberCompleteAnimation);
+            animator = GetComponent<Animator>();
+        }
+
+        private void OnDisable()
+        {
+            onCardSpawn.Unsubscribe(ActivateSpawnAnimation);
+            onDeath.Unsubscribe(ActivateDeathAnimation);
+            onChamberComplete.Unsubscribe(ActivateChamberCompleteAnimation);
+        }
+        
+        private void ActivateDeathAnimation(bool obj)
+        {
+            animator.SetTrigger(OnPlayerDeath);
+        }
+
+        private void ActivateChamberCompleteAnimation()
+        {
+            animator.SetTrigger(OnChamberComplete);
+        }
+
+        private void ActivateSpawnAnimation()
+        {
+            animator.SetTrigger(OnCardSpawn);
+        }
+    }
+}
